d95830b06bf8af0527b028e64bfca5503467b5e2
Flarg <68232536+flargus@users.noreply.github.com>
2022-06-18 14:29:09 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,15 +4,16 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	nDesnsity = createSlider(100, 1000);
 	nDesnsity.position(0, 0);
-	scrollSpeed = createSlider(1, 30);
+	scrollSpeed = createSlider(1, 12);
 	scrollSpeed.position(0, 20);
+	frameRate(60);
 }
 
 let lanes = [];
 let lanePos = [];
 let timer = 0;
 let notes = [];
-let notemap = [[],[],[],[]];
+let notemap = [[], [], [], []];
 let jCircle = [];
 let kp = [false, false, false, false];
 let tColor = (255, 0, 255);
@@ -21,6 +22,7 @@ let jColor = (255, 255, 255);
 
 function draw() {
 	resizeCanvas(windowWidth, windowHeight);
+	let fps = frameRate();
 	let foo = [];
 	let bar = [];
 	let vs = [];
@@ -51,37 +53,44 @@ function draw() {
 		fill(255, 255, 255);
 	}
 	fill(255, 255, 255);
-	for(let collum of notemap){
+	for (let collum of notemap) {
 		for (let note of collum) {
-			push();
-			console.log(note)
 			fill(0, 255, 255);
 			ellipse(note.x, note.y, note.w, note.h);
 			note.y += scrollSpeed.value();
-			pop();
+			note.
 		}
 	}
 
 	if (millis() >= nDesnsity.value() + timer) {
+		fps = frameRate();
 		dropCircle();
 		timer = millis();
 	}
 	for (let i = 0; i < 4; i++) {
 		kp[i] = false;
 	}
-	if (notemap.length >= 40) {
-		notemap.shift();
-	}
 	lanes = foo;
 	lanePos = bar;
 	jCircle = vs;
+	text('FPS: ' + fps.toFixed(0), -450, -200);
+	fill(255, 255, 255);
+	text(
+		'secs: ' + (millis() / 1000).toFixed(0),
+		-450,
+		-220
+	);
 }
 
 function keyPressed() {
 	if (key == 'd') {
-		keys[0] = true
-
-	};
+		console.log(notemap[0]);
+		let collum = notemap[0];
+		if (jCircle[0].y - collum[collum.length - 1].y < 50) {
+			collum[0].splice(collum[collum.length - 1]);
+		}
+		keys[0] = true;
+	}
 	if (key === 'f') keys[1] = true;
 	if (key === 'j') keys[2] = true;
 	if (key === 'k') keys[3] = true;
@@ -95,7 +104,7 @@ function keyReleased() {
 }
 
 function dropCircle() {
-	let collum =Math.floor(random(0, 4));
+	let collum = Math.floor(random(0, 4));
 	note = {
 		x: lanePos[collum],
 		y: -height / 2,
@@ -103,5 +112,5 @@ function dropCircle() {
 		h: windowWidth * 0.045,
 		color: (0, 225, 255)
 	};
-	notemap.push([collum, note]);
+	notemap[collum].push(note);
 }
