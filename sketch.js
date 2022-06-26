2891ada4db6bdc49d62361464e4f70eedc8b5405
flargus <tomjedraper@icloud.com>
2022-06-26 14:40:22 +0100

    moved back to old hitreg

--- sketch.js
+++ sketch.js
@@ -313,28 +313,28 @@ function keyReleased() {
 function handleKeyPress(key) {
 	keys[key] = true;
 	let column = notemap[key];
-	if (canHit[key].length > 0) {
-		hitSound.play();
-		hit(dist);
-		column.splice(0, 1);
-		notemap[column].splice(0, 1);
-	}
-
-	// for (let i = 0; i < column.length; i++) {
-	// 	let dist = 400 - column[i].y;
-	// 	if (dist < 200) {
-	// 		if (column[i].type === 'note') {
-	// 			hitSound.play();
-	// 			hit(dist);
-	// 			column.splice(i, 1);
-	// 		} else if (column[i].type === 'slider') {
-	// 			console.log('here');
-	// 			column[i].active = true;
-	// 			hitSound.play();
-	// 			hit(dist);
-	// 		}
-	// 	}
+	// if (canHit[key].length > 0) {
+	// 	hitSound.play();
+	// 	hit(dist);
+	// 	column.splice(0, 1);
+	// 	notemap[column].splice(0, 1);
 	// }
+
+	for (let i = 0; i < column.length; i++) {
+		let dist = 400 - column[i].y;
+		if (dist < 200) {
+			if (column[i].type === 'note') {
+				hitSound.play();
+				hit(dist);
+				column.splice(i, 1);
+			} else if (column[i].type === 'slider') {
+				console.log('here');
+				column[i].active = true;
+				hitSound.play();
+				hit(dist);
+			}
+		}
+	}
 }
 
 function handleKeyRelease(key) {
